c2fe0fe87ab0c8d8403496a4892d0bb7ce5ed533
gamze <gamzekandara@gmail.com>
2024-05-14 17:44:53 -0400

    Adds anagram.py

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words_list):
+        anagrams = []
+        for word in words_list:
+            # Check if the sorted letters of the word match the sorted letters of the instance word
+            if sorted(word) == sorted(self.word):
+                anagrams.append(word)
+        return anagrams
+
+
+listen = Anagram("listen")
+words_list = ["enlist", "silent", "tinsel", "apple", "banana"]
+print(listen.match(words_list))
